afc2ab76ba8c4d364e4a74c525eb7566ddc5ede3
alex95cooper <lex95bond@gmail.com>
2021-10-29 12:58:48 +0300

    initial commit

---
+++ FileManager/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace FileManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = "Hello";
+        }
+    }
+}
